5fe2f367745ca7a6dbba96e8eb5fa3112076b214
Brandon Ritchie <64935969+brandonritchie@users.noreply.github.com>
2022-07-20 07:23:22 -0600

    .

--- run_stock.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-"C:\Users\britchie\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.10\Python 3.10 (64-bit).lnk" "C:\Users\britchie\OneDrive - AgReserves, Inc\Documents\stock\run_pred.py"
